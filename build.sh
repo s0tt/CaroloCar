6bda4986cf9af135a5b2be997c958ef3b38c38fd
Thomas Monninger <t.monninger@t-online.de>
2018-03-01 12:18:10 +0100

    Adapted build script to support Windows

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+exit_show() 
+{
+    read -n 1 -s -r -p "Press any key to exit..."
+    exit
+}
+
 usage="$(basename "$0") [-h] [-c] [-d] -- builds the source of carolocup
 
 where:
@@ -10,52 +16,72 @@ where:
 DEBUG=false
 
 while getopts '::chd' option; do
-   case "$option" in
-      h) echo "$usage"
-         exit
-         ;;
-      c) rm -rf build bin
-         ;;
-      d) DEBUG=true
-         ;;
-      :) printf "missing argument for -%s\n" "$OPTARG" >&2
-         echo "$usage" >&2
-         exit 1
-         ;;
-     \?) printf "illegal option: -%s\n" "$OPTARG" >&2
-         echo "$usage" >&2
-         exit 1
-         ;;
-   esac
+    case "$option" in
+		h) echo "$usage" >&2;
+		    exit
+		    ;;
+	    c) rm -rf build bin
+		    ;;
+	    d) DEBUG=true
+		    ;;
+	    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+		    echo "$usage" >&2
+		    exit 1
+		    ;;
+	   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+		    echo "$usage" >&2
+		    exit 1
+		    ;;
+    esac
 done
 shift $((OPTIND - 1))
 
+case "$OSTYPE" in
+    linux*)  
+    {
+        if [ $DEBUG = true ]; then 
+            echo "Linux OS detected" >&2;
+        fi
+    } ;;
+    msys*)
+    {	
+        if [ $DEBUG = true ]; then 
+            echo "Windows OS detected" >&2;
+        fi
+	} ;;
+    *)       
+    {
+        echo "Unsupported OS" >&2;
+    } ;;
+esac
+
 if [ $DEBUG = true ]; then 
-   echo "Options processed" >&2;
+    echo "Options processed" >&2;
 fi
 
 mkdir -p build bin
 
 if [ $DEBUG = true ]; then 
-   echo "Folders build and bin created" >&2;
+    echo "Folders build and bin created" >&2;
 fi
 
 cd build
 
 if [ $DEBUG = true ]; then 
-   echo "Call cmake" >&2;
+    echo "Call cmake" >&2;
 fi
 
 cmake -DDEBUG:BOOL=$DEBUG ../src
 
 if [ $DEBUG = true ]; then 
-   echo "Call make" >&2;
+    echo "Call make" >&2;
 fi
 
-make
-
-if [ $DEBUG = true ]; then 
-   echo "Copy executable to bin" >&2;
+if [ $OSTYPE = "linux" ]; then 
+	make
+	cp carolocup ../bin/carolocup
+elif [ $OSTYPE = "msys" ]; then 
+	echo "TODO auto build under Windows" >&2;
 fi
 
-cp carolocup ../bin/carolocup
+exit_show
